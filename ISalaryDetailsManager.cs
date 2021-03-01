be1f1de5f5684746d8fe9f0ddb33eebf40da2f30
laddigam <59866642+laddigam@users.noreply.github.com>
2021-03-01 19:20:20 +0530

    Add files via upload

---
+++ ISalaryDetailsManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Joho.Services.DTO.Response;
+using System.Threading.Tasks;
+using Joho.Services.Entities.Models;
+
+namespace Joho.Services.Abstract
+{
+   
+    
+    
+    public interface  ISalaryDetailsManager
+    {
+        public Task<IEnumerable<SalaryInfoRes>> GetSalaryDetailsList(int sId);
+        public Task<IList<SalaryInfoRes>> GetAllSalaryDetails();
+        public Task<IList<usersalInfo>> GetUserSalaryDetails();
+        public Task<CommonRespose> SaveSalaryDetails(SalaryInfoReq salaryInfoReq);
+        public Task<CommonRespose> UpdateSalaryDetailsById(SalaryInfoReq salaryInfoReq);
+        public Task<CommonRespose> DeleteSalryDetailsById(int sId);
+    }
+}
+
